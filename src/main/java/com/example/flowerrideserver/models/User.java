3644b490f5e0d14d505bffe9a8413ee2b7d24398
michu-dunat <kontaktmichaldunat@gmail.com>
2021-11-16 13:43:13 +0100

    add User model

---
+++ src/main/java/com/example/flowerrideserver/models/User.java
@@ -0,0 +1,29 @@
+package com.example.flowerrideserver.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String login;
+    private String password;
+    private String role;
+
+    public User(String login, String password, String role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+}
