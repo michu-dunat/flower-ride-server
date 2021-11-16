f370c816fda3409951e054b26ef1ebbf184e4ac1
michu-dunat <kontaktmichaldunat@gmail.com>
2021-11-16 13:22:50 +0100

    add initial project files

---
+++ src/main/java/com/example/flowerrideserver/FlowerRideServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowerrideserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerRideServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerRideServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flowerrideserver/FlowerRideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowerrideserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerRideServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
